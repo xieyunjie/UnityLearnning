8a0e5d337838fbd44890a98837c9ae03ecc898a8
xieyunjie <yunjie.xie@hotmail.com>
2017-04-17 12:21:45 +0800

    加入拦截器后测试

--- UnityWeb/App_Start/UnityConfig.cs
+++ UnityWeb/App_Start/UnityConfig.cs
@@ -37,7 +37,7 @@ namespace UnityWeb.App_Start
 
         /// <summary>Registers the type mappings with the Unity container.</summary>
         /// <param name="container">The unity container to configure.</param>
-        /// <remarks>There is no need to register concrete types such as controllers or API controllers (unless you want to 
+        /// <remarks>There is no need to register concrete types such as controllers or API controllers (unless you want to
         /// change the defaults), as Unity allows resolving a concrete type even if it was not previously registered.</remarks>
         public static void RegisterTypes(IUnityContainer container)
         {
@@ -61,7 +61,11 @@ namespace UnityWeb.App_Start
 
             container.AddNewExtension<Interception>();
             container.RegisterType<IUnityOfWork, UnityOfWork>(new PerRequestLifetimeManager());
+            //加入拦截器，则在Resolve时，相当于每次生成一次对象，此时对象hashcode是不一致的。
+            //但，使用Resolve对象执行方法时，unity拦截器会重新按设定的规则生成执行对象，然后使用该对象执行方法。
+            //则Resolve对象与执行方法对象是不一致的。
             container.RegisterType<IEmpService, EmpService>(new PerRequestLifetimeManager(),new Interceptor<InterfaceInterceptor>(),new InterceptionBehavior<LoggingInterceptionBehavior>());
+            //container.RegisterType<IEmpService, EmpService>(new ContainerControlledLifetimeManager(), new Interceptor<InterfaceInterceptor>(), new InterceptionBehavior<LoggingInterceptionBehavior>());
 
         }
     }
--- UnityWeb/Controllers/UnityController.cs
+++ UnityWeb/Controllers/UnityController.cs
@@ -14,7 +14,7 @@ namespace UnityWeb.Controllers
         private IEmpService empSvc = null;
         public UnityController(IEmpService vc)
         {
-            this.empSvc = UnityLearnning.UContainer.Instanst.Resolve<IEmpService>(); 
+            this.empSvc = UnityLearnning.UContainer.Instanst.Resolve<IEmpService>();
         }
         // GET: Unity
         public ActionResult Index()
@@ -24,14 +24,38 @@ namespace UnityWeb.Controllers
 
         public string GetHash()
         {
-            empSvc.printUnityOfWork();
-            empSvc.printNewUnitOfWork();
+            this.empSvc.printUnityOfWork();
+            System.Diagnostics.Trace.WriteLine("this.empSvc hashcode:" + this.empSvc.GetHashCode());
+
+            var e = UnityLearnning.UContainer.Instanst.Resolve<IEmpService>();
+            System.Diagnostics.Trace.WriteLine("e hashcode:" + e.GetHashCode());
+            e.printUnityOfWork();
+
             return string.Format("controller hash:{0}, empsvc hash:{1}", this.GetHashCode().ToString(),this.empSvc.GetHashCode()) ;
+
         }
 
         public string GetHash2()
         {
            var e = UnityLearnning.UContainer.Instanst.Resolve<EmpService>();
+            return string.Format("controller hash:{0}, empsvc hash:{1}", this.GetHashCode().ToString(), e.GetHashCode());
+        }
+
+        public string GetHash3()
+        {
+            System.Diagnostics.Trace.WriteLine("this.empSvc hashcode:" + this.empSvc.GetHashCode());
+
+            var e = UnityLearnning.UContainer.Instanst.Resolve<IEmpService>();
+            System.Diagnostics.Trace.WriteLine("e hashcode:" + e.GetHashCode());
+
+            var e2 = UnityLearnning.UContainer.Instanst.Resolve<IEmpService>();
+            System.Diagnostics.Trace.WriteLine("e2 hashcode:" + e2.GetHashCode());
+
+
+            e.printUnityOfWork();
+            e2.printUnityOfWork();
+            empSvc.printUnityOfWork();
+
             return string.Format("controller hash:{0}, empsvc hash:{1}", this.GetHashCode().ToString(), e.GetHashCode());
         }
 
